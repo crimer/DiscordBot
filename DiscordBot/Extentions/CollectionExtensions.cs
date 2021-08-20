b3f284332980d64122bb8fef22200d040bc44fdc
shevchenko nikita <99nik.pro@gmail.com>
2021-08-20 19:03:39 +1000

    Вспомогающие классы

---
+++ DiscordBot/Extentions/CollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DiscordBot.Extentions
+{
+    /// <summary>
+    /// Методы расширения для коллекций
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Коллекция пуста или null
+        /// </summary>
+        /// <param name="list">Коллекция</param>
+        /// <returns>Bool</returns>
+        public static bool IsNullOrEmpty(this IList list) => list == null || list.Count == 0;
+        
+        /// <summary>
+        /// Получить рандомный элемент колекции
+        /// </summary>
+        /// <param name="items">Коллекция</param>
+        /// <typeparam name="T">Тип коллекции</typeparam>
+        /// <returns>Элемент коллекции</returns>
+        public static T Random<T>(this IList<T> items)
+        {
+            var random = new Random();
+            return items[random.Next(items.Count)];
+        }
+    }
+}
---
+++ DiscordBot/Utils/EmbedHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Discord;
+
+namespace DiscordBot.Utils
+{
+    /// <summary>
+    /// Класс работы с Embed
+    /// </summary>
+    public static class EmbedHelper
+    {
+        /// <summary>
+        /// Создать базовый Embed
+        /// </summary>
+        /// <param name="title">Заголовок</param>
+        /// <param name="description">Описание</param>
+        /// <param name="color">Цвет</param>
+        /// <returns>Embed</returns>
+        public static Embed CreateBasicEmbed(string title, string description, Color color)
+        {
+            return new EmbedBuilder()
+                .WithTitle(title)
+                .WithDescription(description)
+                .WithColor(color)
+                .WithCurrentTimestamp().Build();
+        }
+
+        /// <summary>
+        /// Создать Embed ошибки
+        /// </summary>
+        /// <param name="ex">Ошибка</param>
+        /// <returns>Embed</returns>
+        public static Embed CreateErrorEmbed(Exception ex)
+        {
+            return new EmbedBuilder()
+                .WithTitle($"Ошибка в - {ex.Source}")
+                .WithDescription($"**Описание**: \n{ex.Message}")
+                .WithColor(Color.DarkRed)
+                .WithCurrentTimestamp().Build();
+        }
+    }
+}
